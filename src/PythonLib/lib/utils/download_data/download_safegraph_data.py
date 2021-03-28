f2febd402b0d6c900e66b7a11007ec8b11405db6
Angel <53478143+007sya@users.noreply.github.com>
2021-03-28 14:35:39 +0200

    Added SafeGraphSession class

--- src/PythonLib/lib/utils/download_data/download_safegraph_data.py
+++ src/PythonLib/lib/utils/download_data/download_safegraph_data.py
@@ -1,29 +1,41 @@
 # %%
 import boto3
-from utils.secrets.secrets import (SAFEGRAPH_ACCESS_KEY_ID,
-                                   SAFEGRAPH_SECRET_ACCESS_KEY)
+from utils.secrets.secrets import (SAFEGRAPH_ACCESS_KEY_ID, SAFEGRAPH_BUCKET,
+                                   SAFEGRAPH_ENDPOINT_URL,
+                                   SAFEGRAPH_REGION_NAME,
+                                   SAFEGRAPH_SECRET_ACCESS_KEY,
+                                   SAFEGRAPH_SERVICE_NAME)
 
 
 # %%
-class SafeGrahSession():
-    def __init__(self, prefix):
+class SafeGraphSession():
+    def __init__(self, prefix, bucket_name):
         self.access_key_id = SAFEGRAPH_ACCESS_KEY_ID
         self.secret_access_key = SAFEGRAPH_SECRET_ACCESS_KEY
+        self.service_name = SAFEGRAPH_SERVICE_NAME
+        self.bucket_name = SAFEGRAPH_BUCKET
+        self.endpoint = SAFEGRAPH_ENDPOINT_URL
+        self.region_name = SAFEGRAPH_REGION_NAME
+
         self.prefix = prefix
 
+        self.session = boto3.Session(
+            aws_access_key_id=self.access_key_id,
+            aws_secret_access_key=self.secret_access_key,
+            region_name=self.region_name)
+        self.client = self.session.client(self.service_name,
+                                          endpoint_url=self.endpoint)
+        self.bucket = self.session.resource(self.service_name,
+                                            endpoint_url=self.endpoint).Bucket(bucket_name)
 
-session = boto3.Session(
-    aws_access_key_id=SAFEGRAPH_ACCESS_KEY_ID,
-    aws_secret_access_key=SAFEGRAPH_SECRET_ACCESS_KEY)
 
-bucket = 'sg-c19-response'
 prefix = 'monthly-patterns-2020-12'
-s3 = session.client('s3', endpoint_url='https://s3.wasabisys.com')
+bucket = "sg-c19-response"
+session = SafeGraphSession(prefix, bucket)
 # %%
 
 # lists all relevant objects in the S3 bucket and stores the paths by date in the date_dict
-s3r = session.resource('s3', endpoint_url='https://s3.wasabisys.com')
-bucket_obj = s3r.Bucket(bucket)
+bucket_obj = session.bucket
 # %%
 files_in_bucket = list(bucket_obj.objects.all())
 # %%
