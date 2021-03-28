5a905e483bd32a2c1f9bdd149a57e38e56d6a8d3
Angel <53478143+007sya@users.noreply.github.com>
2021-03-28 14:12:34 +0200

    Added template for the secrets.py file

--- src/PythonLib/lib/utils/secrets/secrets_template.py
+++ src/PythonLib/lib/utils/secrets/secrets_template.py
@@ -6,6 +6,9 @@ private API tokens, paswords, etc...
 """
 
 SAFEGRAPH_MAIL = "mail@gmail.com"
-SAFEGRAPH_PASSWORD = "password"
-SAFEGRAPH_ACCESS_KEY_ID = "aaaaaaaaaaaaaaaaaaa"
-SAFEGRAPH_SECRET_ACCESS_KEY = "aa111aaaaaaaaaaaaaaaaaaaaaaaaaaa"
+SAFEGRAPH_PASSWORD = "iadasdasd"
+SAFEGRAPH_ACCESS_KEY_ID = "adasdasda"
+SAFEGRAPH_SECRET_ACCESS_KEY = "DIASDADSASDSD"
+SAFEGRAPH_BUCKET = 'asdasd'
+SAFEGRAPH_ENDPOINT_URL = 'https://asdasd.com'
+SAFEGRAPH_SERVICE_NAME = 'iasdads'
