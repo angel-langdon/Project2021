ca699697c14f353b82c9ccaeb43a6c14be73efd8
Angel <53478143+007sya@users.noreply.github.com>
2021-03-28 13:31:32 +0200

    Added secrets_template.py

---
+++ src/PythonLib/lib/utils/secrets/secrets_template.py
@@ -0,0 +1,11 @@
+
+"""
+This is the template for the secrets.py file
+secrets.py file contains sensible information such as
+private API tokens, paswords, etc...
+"""
+
+SAFEGRAPH_MAIL = "mail@gmail.com"
+SAFEGRAPH_PASSWORD = "password"
+SAFEGRAPH_ACCESS_KEY_ID = "aaaaaaaaaaaaaaaaaaa" 
+SAFEGRAPH_SECRET_ACCESS_KEY = "aa111aaaaaaaaaaaaaaaaaaaaaaaaaaa"
