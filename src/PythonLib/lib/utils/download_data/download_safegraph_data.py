d9ce58f807b1fc79cc991eff89574b0f30564621
Angel <53478143+007sya@users.noreply.github.com>
2021-04-02 19:05:06 +0200

    Adding utilities for downloading the data

--- src/PythonLib/lib/utils/download_data/download_safegraph_data.py
+++ src/PythonLib/lib/utils/download_data/download_safegraph_data.py
@@ -1,4 +1,6 @@
 # %%
+import sys
+
 import boto3
 from utils.secrets.secrets import (SAFEGRAPH_ACCESS_KEY_ID, SAFEGRAPH_BUCKET,
                                    SAFEGRAPH_ENDPOINT_URL,
@@ -28,6 +30,9 @@ class SafeGraphSession():
         self.bucket = self.session.resource(self.service_name,
                                             endpoint_url=self.endpoint).Bucket(bucket_name)
 
+    def download_file(self, bucket_path, dest_path):
+        self.client.download_file(self.bucket_name, bucket_path, dest_path)
+
 
 prefix = 'monthly-patterns-2020-12'
 bucket = "sg-c19-response"
@@ -44,9 +49,42 @@ files = [elm.key for elm in files_in_bucket if elm.key.startswith(
     prefix) and (elm.key.endswith('.csv') or elm.key.endswith('.csv.gz'))]
 print(len(files))
 # %%
+session.download_file(files[0], "prueba.csv")
+# %%
+
+
+def filter_files_by_date(files):
+    date_dict = {}
+    for f in files:
+        s = f.split('/')
+        if 'backfill' in s[1]:
+            date = s[-3] + '-' + s[-2]
+            if date in date_dict.keys():
+                date_dict[date] = date_dict[date] + [f]
+            else:
+                date_dict[date] = [f]
+        else:
+            m = int(s[-4])
+            y = s[-5]
+            if m == 1:
+                m = '12'
+                y = str(int(y)-1)
+            elif m > 10:
+                m = str(m-1)
+            else:
+                m = '0' + str(m-1)
+            date = y + '-' + m
+            if date in date_dict.keys():
+                date_dict[date] = date_dict[date] + [f]
+            else:
+                date_dict[date] = [f]
+    return date_dict
+
+
+res = filter_files_by_date(files)
 
 # %%
 
-""" Download geometry data
+res["2021-02"]
 
-"""
+# %%
