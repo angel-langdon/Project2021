c88111dc5041d79f118988c44b525d3337c1f2c2
Angel <53478143+007sya@users.noreply.github.com>
2021-04-07 19:22:58 +0200

    Added file utils

---
+++ src/PythonLib/lib/utils/file_utils/file_type.py
@@ -0,0 +1,2 @@
+def is_mobility_pattern(file_path: str) -> bool:
+    return "patterns-part" in file_path
---
+++ src/PythonLib/lib/utils/file_utils/file_utils.py
@@ -0,0 +1,10 @@
+import os
+from typing import Iterable
+
+from utils.path_utils import path_utils
+
+
+def save_list_in_text_file(file_name: str, iterable: Iterable):
+    path_utils.create_dir_if_necessary(file_name)
+    with open(file_name, "w") as f:
+        f.writelines(l + "\n" for l in iterable)
