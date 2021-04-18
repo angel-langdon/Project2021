90207a57eeab3a740ac4b2ed234512117a9c8732
Angel <53478143+007sya@users.noreply.github.com>
2021-04-18 14:38:21 +0200

    Added new utils for detecting file type

--- src/PythonLib/lib/utils/file_utils/file_type.py
+++ src/PythonLib/lib/utils/file_utils/file_type.py
@@ -1,2 +1,19 @@
 def is_mobility_pattern(file_path: str) -> bool:
     return "patterns-part" in file_path
+
+
+def is_brand_info(file):
+    return "brand_info" in file
+
+
+def is_core_poi(file):
+    return "core_poi" in file
+
+
+def get_file_type(file):
+    if is_brand_info(file):
+        return "brand_info"
+    if is_core_poi(file):
+        return "core_poi"
+    if is_mobility_pattern(file):
+        return "mobility_patter"
