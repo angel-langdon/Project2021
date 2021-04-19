b25d6b4021096ed2c3fc62d296a90738699407bc
Angel <53478143+007sya@users.noreply.github.com>
2021-04-19 08:44:57 +0200

    Added new data types

--- src/PythonLib/lib/utils/download_data/data_dtypes.py
+++ src/PythonLib/lib/utils/download_data/data_dtypes.py
@@ -1,15 +1,11 @@
 data_dtypes = {}
-mobility_dtypes = {"placekey": "object",
-                   "safegraph_place_id": "object",
-                   "parent_placekey": "object",
-                   "parent_safegraph_place_id": "object",
-                   "location_name": "category",
+mobility_dtypes = {"location_name": "category",
                    "street_address": "category",
                    "city": "category",
                    "region": "category",
-                   "postal_code": "int32",
-                   "safegraph_brand_ids": "object",
                    "poi_cbg": "category",
                    "brands": "category",
                    "date_range_start": "category",
                    "date_range_end": "category"}
+
+census_dtypes = {"census_block_group": "category"}
