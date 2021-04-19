5d4c7b537cc11c829817deb28ac66318b80f8f6e
Angel <53478143+007sya@users.noreply.github.com>
2021-04-19 08:45:12 +0200

    Added new datasets

--- src/PythonLib/lib/utils/download_data/datasets.py
+++ src/PythonLib/lib/utils/download_data/datasets.py
@@ -2,6 +2,8 @@
 import os
 
 import pandas as pd
+from utils.download_data import data_dtypes as dtypes
+from utils.file_utils import file_type
 from utils.path_utils import path_utils, paths
 
 
@@ -33,6 +35,30 @@ def get_core_poi_by_city(city, region=None, save_data=True):
         final_df.to_csv(file_name, encoding="utf-8", index=False)
     return final_df
 
-#get_core_poi_by_city("Houston", "TX")
+
+# %%
+files = [f for f in path_utils.list_files_recursively(paths.open_census_dir)
+         if file_type.is_census_data(f)]
+houston_folder = os.path.join(paths.processed_datasets, "Houston")
+subway_normalized = os.path.join(houston_folder, "subway_normalized.csv")
+df_normalized = pd.read_csv(subway_normalized, dtype=dtypes.mobility_dtypes)
+# %%
+file = files[0]
+census = pd.read_csv(file, dtype=dtypes.census_dtypes)
+# %%
+census[census["census_block_group"].isin(df_normalized["poi_cbg"])]
+# %%
+
+
+def get_census_metadata():
+    files = [f for f in path_utils.list_files_recursively(paths.open_census_dir)
+             if file_type.is_census_metadata(f)]
+    for file in files:
+        if "description" in file:
+            break
+    return pd.read_csv(file)
+
+
+# %%
 
 # %%
