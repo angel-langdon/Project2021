a803172b3139eaa747d2c57ef19ae454cd46487e
Angel <53478143+007sya@users.noreply.github.com>
2021-05-05 14:06:15 +0200

    Fixed typo

--- src/web/front/public/data/transform_data.py
+++ src/web/front/public/data/transform_data.py
@@ -10,7 +10,7 @@ def read_csv_data():
     columns = [col for col in df.columns if col != "index"]
     df = df[columns]
     df["date"] = pd.to_datetime(df["date"])
-    df["prediciton"] = df["prediction"].astype(int)
+    df["prediction"] = df["prediction"].astype(int)
     df = df.drop(columns=["safegraph_place_id"])
     df = df.dropna()
     return df
@@ -19,7 +19,7 @@ def read_csv_data():
 df = read_csv_data()
 df = df[df["date"] > (df["date"].max() - timedelta(days=31))]
 # %%
-df.to_json("data.json", "records", indent=4)
+df.to_json("subway.json", "records", indent=4)
 
 # %%
 df
